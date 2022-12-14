f347fa4a3c23a93c6b6428a33a04ea83324978fa
Jose Cortinas <jacortinas@gmail.com>
2022-12-14 05:52:54 +0000

    Day 12 solved

--- 2022/12/sol.rb
+++ 2022/12/sol.rb
@@ -1,57 +1,96 @@
-require "pry"
+require "set"
 
-SAMPLE = <<~EOS
-  Sabqponm
-  abcryxxl
-  accszExk
-  acctuvwj
-  abdefghi
-EOS
+input = File.read('input.in')
 
-class Grid
-  attr_reader :start_pos, :end_pos, :map
+start_pos = end_pos = nil
 
-  def initialize(input)
-    @start_pos = @end_pos = nil
-
-    @map = input.lines(chomp: true).each_with_index.map do |line, y|
-      line.chars.each_with_index.map do |char, x|
-        if char == "S"
-          @start_pos = [x, y]
-          1
-        elsif char == "E"
-          @end_pos = [x, y]
-          26
-        else
-          char.ord - 96
-        end
-      end
+heights = input.lines(chomp: true).each_with_index.map do |line, y|
+  line.chars.each_with_index.map do |char, x|
+    if char == "S"
+      start_pos = [x, y]
+      1
+    elsif char == "E"
+      end_pos = [x, y]
+      26
+    else
+      char.ord - 96
     end
-  end
-
-  def neighbors(x, y)
-    [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]].select do |x, y|
-      x >= 0 && y >= 0 && x < @map.first.size && y < @map.size
-    end
-  end
-
-  def to_s
-    output = []
-
-    divider_line = "+#{'-' * ((@map[0].size * 3) + 7)}+"
-
-    output << divider_line
-
-    @map.each do |line|
-      output << "|#{line.map { |v| v.to_s.rjust(3) }.join('|')}|"
-      output << divider_line
-    end
-
-    output.join("\n")
   end
 end
 
-input = SAMPLE
-grid = Grid.new(input)
+puts "#{heights.first.size}x#{heights.size} - #{heights.first.size * heights.size}"
+puts "Start: #{start_pos.inspect} - End: #{end_pos.inspect}"
 
-puts grid.to_s
+queue = [start_pos]
+seen = Set.new
+dists = { start_pos => 0 }
+
+until queue.empty?
+  cx, cy = *queue.shift
+
+  seen << [cx, cy]
+
+  if cx == end_pos[0] && cy == end_pos[1]
+    puts "Part 1: #{dists[[cx, cy]]}"
+    break
+  end
+
+  [[cx + 1, cy], [cx, cy + 1], [cx - 1, cy], [cx, cy - 1]].each do |nx, ny|
+    next if nx.negative? || ny.negative?
+    next if nx >= heights.first.size
+    next if ny >= heights.size
+    next if seen.include?([nx, ny])
+    next if heights[ny][nx] - heights[cy][cx] > 1
+
+    queue << [nx, ny] unless queue.include?([nx, ny])
+
+    dists[[nx, ny]] = dists[[cx, cy]] + 1
+  end
+end
+
+# ---
+
+start_pos = nil
+
+heights = input.lines(chomp: true).each_with_index.map do |line, y|
+  line.chars.each_with_index.map do |char, x|
+    if char == "S"
+      1
+    elsif char == "E"
+      start_pos = [x, y]
+      26
+    else
+      char.ord - 96
+    end
+  end
+end
+
+puts "#{heights.first.size}x#{heights.size} - #{heights.first.size * heights.size}"
+puts "Start: #{start_pos.inspect}"
+
+queue = [start_pos]
+seen = Set.new
+dists = { start_pos => 0 }
+
+until queue.empty?
+  cx, cy = *queue.shift
+
+  seen << [cx, cy]
+
+  if heights[cy][cx] == 1
+    puts "Part 2: #{dists[[cx, cy]]}"
+    break
+  end
+
+  [[cx + 1, cy], [cx, cy + 1], [cx - 1, cy], [cx, cy - 1]].each do |nx, ny|
+    next if nx.negative? || ny.negative?
+    next if nx >= heights.first.size
+    next if ny >= heights.size
+    next if seen.include?([nx, ny])
+    next if heights[cy][cx] - heights[ny][nx] > 1
+
+    queue << [nx, ny] unless queue.include?([nx, ny])
+
+    dists[[nx, ny]] = dists[[cx, cy]] + 1
+  end
+end
